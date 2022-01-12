a4977c13a34db9e77aa525e8e04b4c9a4fbd790a
sam <sgeek019@gmail.com>
2022-01-12 10:19:52 +0200

    add datetime

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from telethon import TelegramClient
+from datetime import datetime
+from datetime import timedelta
 
-chat = 'me'
+chat = 'satisface'
 api_id = '17504125'
 api_hash = '35edac267afc648dfd6ded76ed9b4462'
 
@@ -8,29 +10,41 @@ client = TelegramClient('anon', api_id, api_hash)
 
 async def main():
     
-    f = open('debug.txt', 'a', encoding='utf-8')
     
-    for message in await client.get_messages('satisface', limit=100):
     
-        if message.from_id == None:
-            fromUser = 'satisface'
-        else:
-            fromUser = 'sam.cpp'
-            
-        if type(message.text) == str:
-            text = message.text
-        else:
-            text = ' '
-            
-        msg = fromUser + '|' + text
+    for i in range(10):
+    
+        fileName = chat+'_'+str(i)+'.txt'
+        f = open(fileName, 'a', encoding='utf-8')
         
-        if message.media != None:
-            attacPath = './'+str(message.id)+'.png'
-            msg = msg + '|' + attacPath
-            #await client.download_media(message.media,attacPath)
-        f.write(msg+'\n')
+        tmpMsg = await client.get_messages(chat, limit=1)
+        
+        lastId = tmpMsg[0].id
     
-    f.close();
+        for message in await client.get_messages(chat, limit=200, offset_id=lastId - i*200):
+        
+            if message.from_id == None:
+                fromUser = 'satisface'
+            else:
+                fromUser = 'sam.cpp'
+                
+            dateMsg = str(message.date)[:-6]
+            dateObj = datetime.strptime(dateMsg, '%Y-%m-%d %H:%M:%S') + timedelta(hours=2)
+                
+            if type(message.text) == str:
+                text = message.text
+            else:
+                text = ' '
+                
+            msg = str(dateObj) + '|' + fromUser + '|' + text
+            
+            if message.media != None:
+                attacPath = './'+str(message.id)+'.png'
+                msg = msg + '|' + attacPath
+                #await client.download_media(message.media,attacPath)
+            f.write(msg+'\n')
+            
+        f.close()
         
 with client:
     client.loop.run_until_complete(main())
--- test3.py
+++ test3.py
@@ -4,7 +4,10 @@ from telethon import TelegramClient
 
 from json import dumps as json_dumps
 
-chat = 'me'
+from datetime import datetime
+from datetime import timedelta
+
+chat = 'satisface'
 api_id = '17504125'
 api_hash = '35edac267afc648dfd6ded76ed9b4462'
 
@@ -12,9 +15,12 @@ client = TelegramClient('anon', api_id, api_hash)
 
 async def main():
     
-    async for dialog in client.iter_dialogs():
-            print(dialog)
-            print('\n')
+        tmpMsg = await client.get_messages(chat, limit=1)
+        #print(tmpMsg[0].date)
+        dateMsg = str(tmpMsg[0].date)[:-6]
+        dateObj = datetime.strptime(dateMsg, '%Y-%m-%d %H:%M:%S') + timedelta(hours=2)
+        
+        print(str(dateObj))
         
 with client:
     client.loop.run_until_complete(main())
